f971626921596ef49ab2710e8a9789071447dfaa
Howard Tang <tangh@google.com>
2013-12-31 16:07:05 +0000

    Preserve base url.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var cheerio = require('cheerio');
 var request = require('request');
-var base_url = 'http://proxybay.eu/search/';
+var base_url = 'http://proxybay.eu';
 
 var search = function(query, cb) {
-	request(base_url + query, function(err, response, body) {
+	request(base_url + '/search/' + query, function(err, response, body) {
 		if (!err) {
 			$ = cheerio.load(body);
 			var result = [];
 			$('#searchResult .detName > a').each(function() {
-				result.push(this.attr('href'));
+				result.push(base_url + this.attr('href'));
 			});
 			cb(result);
 		}
